28913288e369b5d8ba6bbccf22ad1ed7b9142253
DevTeisrud <developer@teisrud.no>
2022-08-30 00:27:18 +0200

    new xcode environment

---
+++ CurriculumVitaeTeisrud/CurriculumVitaeTeisrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurriculumVitaeTeisrudApp.swift
+//  CurriculumVitaeTeisrud
+//
+//  Created by Bjørn Kristian Teisrud on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurriculumVitaeTeisrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
