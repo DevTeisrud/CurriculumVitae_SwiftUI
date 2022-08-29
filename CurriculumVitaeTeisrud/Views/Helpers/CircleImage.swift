78f39d9a57f916e1d76f8b2a25db49e4fe9e7a10
DevTeisrud <developer@teisrud.no>
2022-08-30 01:33:21 +0200

    Fresh new designed ProfilePage

---
+++ CurriculumVitaeTeisrud/Views/Helpers/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  CurriculumVitaeTeisrud
+//
+//  Created by Bjørn Kristian Teisrud on 30/08/2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .frame(width: 200, height: 200)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("UiALogoShort"))
+    }
+}
--- CurriculumVitaeTeisrud/Views/Profile/ProfilePage.swift
+++ CurriculumVitaeTeisrud/Views/Profile/ProfilePage.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ProfilePage: View {
     var body: some View {
-        Text("ProfilePage")
+        ScrollView {
+            MapView(coordinate: CLLocationCoordinate2D(
+                latitude: 58.13635,
+                longitude: 7.99771))
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: Image("BKT"))
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Bjørn Kristian Teisrud")
+                    .font(.title)
+                Text("B.Sc Computer Science")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text("M.Sc Industrial Economics and Technology Management student")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About me")
+                    .font(.title2)
+                Text("TODO: Information About me")
+            }
+            .padding()
+        }
+        .navigationTitle("ProfilePage")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
